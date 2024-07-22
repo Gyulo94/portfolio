6ac688f93bb5b381d17d8fdfa770826240346d1c
gyulo94 <qkrcjfrb112@gmail.com>
2024-07-22 15:19:39 +0900

    페이지 렌더링될 때마다 api 요청

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,7 +6,7 @@ import "./style.css";
 const notionSecret = process.env.NOTION_TOKEN;
 const notionDatabaseId = process.env.NOTION_DATABASE_ID;
 
-export default async function Projects() {
+async function fetchProjects(): Promise<ProjectProps[]> {
   try {
     const response = await axios.post(
       `https://api.notion.com/v1/databases/${notionDatabaseId}/query`,
@@ -15,37 +15,39 @@ export default async function Projects() {
       },
       {
         headers: {
-          accept: "application/json",
-          "Notion-Version": "2022-02-22",
+          Accept: "application/json",
+          "Notion-Version": "2022-06-28",
           "Content-Type": "application/json",
-          authorization: `Bearer ${notionSecret}`,
+          Authorization: `Bearer ${notionSecret}`,
         },
       }
     );
-
-    const projects = response.data.results;
-
-    const projectNames = projects.map((project: ProjectProps) => {
-      return project.properties?.이름?.title[0]?.plain_text || "";
-    });
-
-    console.log("Project Names:", projectNames);
-
-    return (
-      <div id="project-wrapper">
-        <h1 className="project-count">
-          찰규의 프로젝트의 수는?
-          <span style={{ color: "#7925d3" }}> {projectNames.length}</span>
-        </h1>
-        <div className="project-container">
-          {projects.map((project: ProjectProps) => (
-            <ProjectItem key={project.id} {...project} />
-          ))}
-        </div>
-      </div>
-    );
+    return response.data.results;
   } catch (error) {
-    console.error("Error fetching data from Notion:", error);
-    return <div>Error loading projects</div>;
+    console.error("Error fetching projects:", error);
+    return [];
   }
+}
+
+export default async function Projects() {
+  const projects: ProjectProps[] = await fetchProjects();
+
+  const projectNames = projects.map(
+    (project: ProjectProps) =>
+      project.properties?.이름?.title[0]?.plain_text || ""
+  );
+
+  return (
+    <div id="project-wrapper">
+      <h1 className="project-count">
+        찰규의 프로젝트의 수는?
+        <span style={{ color: "#7925d3" }}> {projectNames.length}</span>
+      </h1>
+      <div className="project-container">
+        {projects.map((project: ProjectProps) => (
+          <ProjectItem key={project.id} {...project} />
+        ))}
+      </div>
+    </div>
+  );
 }
