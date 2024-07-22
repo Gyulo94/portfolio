701a55783af6d268fb262aca8fb4574a66153a50
gyulo94 <qkrcjfrb112@gmail.com>
2024-07-22 14:57:01 +0900

    fetch -> axios 변경, 썸네일 css 변경

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,44 +1,51 @@
 import ProjectItem from "@/components/projects";
 import { ProjectProps } from "@/types";
+import axios from "axios";
 import "./style.css";
 
 const notionSecret = process.env.NOTION_TOKEN;
 const notionDatabaseId = process.env.NOTION_DATABASE_ID;
 
 export default async function Projects() {
-  const option = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Notion-Version": "2022-06-28",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${notionSecret}`,
-    },
-    body: JSON.stringify({
-      page_size: 100,
-    }),
-  };
-  const res = await fetch(
-    `https://api.notion.com/v1/databases/${notionDatabaseId}/query`,
-    option
-  );
-  const projects = await res.json();
+  try {
+    const response = await axios.post(
+      `https://api.notion.com/v1/databases/${notionDatabaseId}/query`,
+      {
+        page_size: 100,
+      },
+      {
+        headers: {
+          accept: "application/json",
+          "Notion-Version": "2022-02-22",
+          "Content-Type": "application/json",
+          authorization: `Bearer ${notionSecret}`,
+        },
+      }
+    );
 
-  const projectNames = projects.results.map((project: ProjectProps) => {
-    project.properties?.이름?.title[0]?.plain_text;
-  });
+    const projects = response.data.results;
 
-  return (
-    <div id="project-wrapper">
-      <h1 className="project-count">
-        찰규의 프로젝트의 수는?
-        <span style={{ color: "#7925d3" }}> {projectNames.length}</span>
-      </h1>
-      <div className="project-container">
-        {projects.results.map((project: ProjectProps) => (
-          <ProjectItem key={project.id} {...project} />
-        ))}
+    const projectNames = projects.map((project: ProjectProps) => {
+      return project.properties?.이름?.title[0]?.plain_text || "";
+    });
+
+    console.log("Project Names:", projectNames);
+
+    return (
+      <div id="project-wrapper">
+        <h1 className="project-count">
+          찰규의 프로젝트의 수는?
+          <span style={{ color: "#7925d3" }}> {projectNames.length}</span>
+        </h1>
+        <div className="project-container">
+          {projects.map((project: ProjectProps) => (
+            <ProjectItem key={project.id} {...project} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching data from Notion:", error);
+    return <div>Error loading projects</div>;
+  }
 }
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -14,19 +14,23 @@ export default function ProjectItem(data: ProjectProps) {
   const end = data.properties.작업기간?.date?.end;
 
   const calculatedPeriod = (start: string, end: string): number => {
+    if (!start || !end) {
+      start = "????-??-??";
+      end = "????-??-??";
+    }
     const startDateArray = start.split("-");
     const endDateArray = end.split("-");
 
-    const startYear = parseInt(startDateArray[0], 10);
-    const startMonth = parseInt(startDateArray[1], 10) - 1;
-    const startDay = parseInt(startDateArray[2], 10);
-
-    const endYear = parseInt(endDateArray[0], 10);
-    const endMonth = parseInt(endDateArray[1], 10) - 1;
-    const endDay = parseInt(endDateArray[2], 10);
-
-    const startDate = new Date(startYear, startMonth, startDay);
-    const endDate = new Date(endYear, endMonth, endDay);
+    const startDate = new Date(
+      parseInt(startDateArray[0]),
+      parseInt(startDateArray[1]) - 1,
+      parseInt(startDateArray[2])
+    );
+    const endDate = new Date(
+      parseInt(endDateArray[0]),
+      parseInt(endDateArray[1]) - 1,
+      parseInt(endDateArray[2])
+    );
 
     const diffInMs = Math.abs(endDate.getTime() - startDate.getTime());
     const result = diffInMs / (1000 * 60 * 60 * 24);
@@ -37,21 +41,20 @@ export default function ProjectItem(data: ProjectProps) {
   return (
     <div className="project-card">
       <a href={projectUrl}>
-        <Image
-          src={imgSrc}
-          alt={title}
-          width="512"
-          height="350"
-          layout="responsive"
-          objectFit="none"
-          quality={100}
-        />
-
+        <div className="project-image-wrapper">
+          <Image
+            src={imgSrc}
+            alt={title}
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
         <h1 className="project-title">{title}</h1>
       </a>
       <h3 className="project-description">{description}</h3>
       <a href={github}>깃허브 바로가기</a>
-      <a href={youtube}>유튜브 바로가기</a>
+      {youtube !== null ? <a href={youtube}>유튜브 바로가기</a> : null}
       <h3 className="project-period">
         작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
       </h3>
