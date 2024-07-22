71d4aab91d30930ac12c4b6f57f5b204967e4e83
gyulo94 <qkrcjfrb112@gmail.com>
2024-07-22 15:31:25 +0900

    서버액션 적용

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+const notionSecret = process.env.NOTION_TOKEN;
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+
+export async function GET() {
+  try {
+    const response = await axios.post(
+      `https://api.notion.com/v1/databases/${notionDatabaseId}/query`,
+      {
+        page_size: 100,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+          "Notion-Version": "2022-06-28",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${notionSecret}`,
+        },
+      }
+    );
+
+    return NextResponse.json(response.data.results);
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    return NextResponse.error();
+  }
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,47 +1,40 @@
+"use client";
+
 import ProjectItem from "@/components/projects";
 import { ProjectProps } from "@/types";
 import axios from "axios";
+import { useEffect, useState } from "react";
 import "./style.css";
 
-const notionSecret = process.env.NOTION_TOKEN;
-const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+export default function ClientComponent() {
+  const [projects, setProjects] = useState<ProjectProps[]>([]);
+  const [loading, setLoading] = useState(true);
 
-async function fetchProjects(): Promise<ProjectProps[]> {
-  try {
-    const response = await axios.post(
-      `https://api.notion.com/v1/databases/${notionDatabaseId}/query`,
-      {
-        page_size: 100,
-      },
-      {
-        headers: {
-          Accept: "application/json",
-          "Notion-Version": "2022-06-28",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${notionSecret}`,
-        },
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await axios.get("/api/projects");
+        setProjects(response.data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+        setProjects([]);
+      } finally {
+        setLoading(false);
       }
-    );
-    return response.data.results;
-  } catch (error) {
-    console.error("Error fetching projects:", error);
-    return [];
+    }
+
+    fetchProjects();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-}
-
-export default async function Projects() {
-  const projects: ProjectProps[] = await fetchProjects();
-
-  const projectNames = projects.map(
-    (project: ProjectProps) =>
-      project.properties?.이름?.title[0]?.plain_text || ""
-  );
 
   return (
     <div id="project-wrapper">
       <h1 className="project-count">
         찰규의 프로젝트의 수는?
-        <span style={{ color: "#7925d3" }}> {projectNames.length}</span>
+        <span style={{ color: "#7925d3" }}> {projects.length}</span>
       </h1>
       <div className="project-container">
         {projects.map((project: ProjectProps) => (
