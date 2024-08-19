6ba987f7c608d33afc2d9c7fea182eed95146968
Gyulo94 <qkrcjfrb112@gmail.com>
2024-08-19 09:13:57 +0900

    레이아웃 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,11 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="kr">
-      <body>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <Header />
+      <body>{children}</body>
+      <Footer />
     </html>
   );
 }
